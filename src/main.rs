8dad235283cde7b17730e2cac3a558df98c06dc8
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-13 10:56:59 +0200

    Sketch in struct ItemListRenderer

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ fn main() -> std::io::Result<()> {
 
     let delete = char::from(127);
 
+    let mut renderer = ItemListRenderer::new(&term, &items);
+
     loop {
         let (cursor_x, _) = cursor.pos();
         let key = term.read_key().unwrap();
@@ -24,18 +26,41 @@ fn main() -> std::io::Result<()> {
             Key::Char(ch) => {
                 if ch == delete && cursor_x > 2 {
                     search_term.pop();
-                    refresh_items(&term, &mut cursor, &items, &search_term)?;
+                    renderer.refresh_items(&search_term)?;
                     cursor.move_left(1);
                 }
                 else if ch != delete {
                     search_term.push(ch);
-                    refresh_items(&term, &mut cursor, &items, &search_term)?;
+                    renderer.refresh_items(&search_term)?;
                     cursor.move_right(1);
                 }
             }
 
             _ => {}
         }
+    }
+}
+
+struct ItemListRenderer<'a> {
+    term: &'a Term,
+    items: &'a Vec<&'a str>,
+    cursor: TerminalCursor
+}
+
+impl<'a> ItemListRenderer<'a> {
+    pub fn new(term: &'a Term, items: &'a Vec<&str>) -> ItemListRenderer<'a> {
+        ItemListRenderer {term: term, items: items, cursor: cursor()}
+    }
+
+    pub fn refresh_items(&mut self, search_term: &str) -> std::io::Result<()> {
+        self.cursor.save_position()?;
+        self.cursor.move_down(self.items.len() as u16 + 1);
+
+        self.term.clear_last_lines(self.items.len() + 1)?;
+        render_items(self.term, self.items, &search_term)?;
+        self.cursor.reset_position()?;
+
+        Ok(())
     }
 }
 
