c3dcd77c5827e6a9563066860b1fbd5663593940
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-13 09:39:14 +0200

    Position cursor according to length of search term

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use crossterm_cursor::cursor;
 
 fn main() -> std::io::Result<()> {
     let term = Term::stdout();
-    let mut offset = 1;
     let mut search_term = String::from("");
     term.write_line(&format!("> {}", search_term))?;
     render_items(&term, "")?;
@@ -16,19 +15,14 @@ fn main() -> std::io::Result<()> {
         let key = term.read_key().unwrap();
 
         match key {
-            Key::Escape => {
-                return Ok(());
-            }
+            Key::Escape => { return Ok(()); }
 
             Key::Char(ch) => {
-
-
                 search_term.push(ch);
                 cursor.move_down(3);
             
                 term.clear_last_lines(3)?;
                 term.write_line(&format!("> {}", search_term))?;
-                offset = offset + 1;
                 render_items(&term, &search_term)?;
                 cursor.move_up(3);
                 cursor.move_right(search_term.len() as u16 + 2);
