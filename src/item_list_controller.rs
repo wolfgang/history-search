f7fefa2c88a7543e5c7c21122d0e286c2c5a0682
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-13 18:02:45 +0200

    Rename item_list_ref to item_list

--- src/item_list_controller.rs
+++ src/item_list_controller.rs
@@ -7,27 +7,27 @@ use console::{Key, Term};
 
 pub struct ItemListController<'a> {
     term: &'a Term,
-    item_list_ref: &'a mut ItemList<'a>
+    item_list: &'a mut ItemList<'a>
 }
 
 impl<'a> ItemListController<'a> {
     pub fn new(
         term: &'a Term, 
-        item_list_ref: &'a mut ItemList<'a>) -> ItemListController<'a> {
+        item_list: &'a mut ItemList<'a>) -> ItemListController<'a> {
 
-        ItemListController { term: term, item_list_ref: item_list_ref }
+        ItemListController { term: term, item_list: item_list }
     }
 
     pub fn run(&mut self) -> std::io::Result<()> {
-        self.item_list_ref.render()?;
-        self.item_list_ref.init_cursor()?;
+        self.item_list.render()?;
+        self.item_list.init_cursor()?;
         loop {
             let key = self.term.read_key().unwrap();
 
             match key {
                 Key::Enter => {
-                    self.item_list_ref.clear()?;
-                    let cmd = self.item_list_ref.selected_item();
+                    self.item_list.clear()?;
+                    let cmd = self.item_list.selected_item();
                     println!("Selected: {:}", cmd);
 
                     let shell = env::var_os("SHELL").unwrap();
@@ -39,11 +39,11 @@ impl<'a> ItemListController<'a> {
                     return Ok(());
                 }
                 Key::Escape => { 
-                    return self.item_list_ref.clear()
+                    return self.item_list.clear()
                 }
-                Key::ArrowUp => { self.item_list_ref.change_selection(-1)?; }
-                Key::ArrowDown => { self.item_list_ref.change_selection(1)?; }
-                Key::Char(ch) => { self.item_list_ref.on_character_entered(ch)?; }
+                Key::ArrowUp => { self.item_list.change_selection(-1)?; }
+                Key::ArrowDown => { self.item_list.change_selection(1)?; }
+                Key::Char(ch) => { self.item_list.on_character_entered(ch)?; }
                 _ => {}
             }
         }
