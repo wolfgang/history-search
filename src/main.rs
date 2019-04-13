96b3c06633d2900c0c75df417a9f5eecbc90c61f
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-13 18:48:06 +0200

    First stab at adding actual commands

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@
 mod item_list;
 mod item_list_controller;
 
-
-
-use std::fs::File;
+use std::io::prelude::*;
+use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader};
+use std::env;
 use console::{Term};
 use item_list::ItemList;
 use item_list_controller::ItemListController;
 
 fn main() -> std::io::Result<()> {
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+
+    if !args.is_empty() {
+        let mut file = OpenOptions::new().append(true).open("test.txt")?;
+        let entry = args.join(" ");
+        write!(file, "{}\n", entry)?;
+        println!("Added entry: {}", entry);
+        return Ok(());
+    }
+
+
     let items = read_items("test.txt");
     let term = Term::stdout();
 
