03bffbc569c169de835abdfe1db6f17211e8d763
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-14 10:53:51 +0200

    Initial experiments with testing of the term object

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod item_storage;
 mod item_list;
 mod item_list_controller;
 
+
 use std::env;
 use console::{Term};
 use item_list::ItemList;
@@ -23,4 +24,24 @@ fn main() -> std::io::Result<()> {
     let mut item_list = ItemList::new(&term, &items);
     return ItemListController::new(&term, &mut item_list).run();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::prelude::*;
+
+    #[test]
+    fn term_can_be_initialized() {
+        let mut term = Term::stdout();
+        term.write_line("HELLO").unwrap();
+        write_to_writer(&mut term, b"HELLO AGAIN").unwrap();
+    }
+
+    fn write_to_writer<W: Write>(writer: &mut W, buf: &[u8]) -> std::io::Result<()> {
+        writer.write(buf)?;
+        Ok(())
+
+    }
+}
+
 
