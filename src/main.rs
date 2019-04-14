93386815b697aff079f6d38845717d975c4478bc
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-14 15:14:36 +0200

    Extract function display_help

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,7 @@ fn main() -> std::io::Result<()> {
     item_storage::init();
 
     if !args.is_empty() {
-        if args[0] == "-h" {
-
-            println!("\nUsage: rp [OPTIONS] INPUT");
-            println!("Store arbitrary command given in INPUT.");
-            println!("\nOptions:");
-            println!("  -d      store current directory along with command");
-            println!("  -h      show this message");
-            println!("\n");
-            println!("If no arguments are given, displays a searchable list:");
-            println!("  Enter to execute the selected command,");
-            println!("  Arrow up/down to change selection");
-            println!("  Escape to cancel");
-            println!("\nCommands are stored in ~/.rp/items.txt");
-            
-            return Ok(())
-        }
+        if args[0] == "-h" { return display_help() }
         return item_storage::add_item(&mut args);
     }
 
@@ -39,6 +24,23 @@ fn main() -> std::io::Result<()> {
     let term = Term::stdout();
     let mut item_list = ItemList::new(&term, &items);
     return ItemListController::new(&term, &mut item_list).run();
+}
+
+fn display_help() -> std::io::Result<()> {
+    println!("\nUsage: rp [OPTIONS] INPUT");
+    println!("Store arbitrary command given in INPUT.");
+    println!("\nOptions:");
+    println!("  -d      store current directory along with command");
+    println!("  -h      show this message");
+    println!("\n");
+    println!("If no arguments are given, displays a searchable list:");
+    println!("  Enter to execute the selected command,");
+    println!("  Arrow up/down to change selection");
+    println!("  Escape to cancel");
+    println!("\nCommands are stored in ~/.rp/items.txt");
+
+    return Ok(())
+
 }
 
 #[cfg(test)]
