ee6010146b9b748f8d83b2e525f54e96ceb63eab
wolfgang <wolfgang.deutsch@gmail.com>
2021-04-03 12:54:52 +0200

    Spruce up install script a bit

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-cargo build --release
+Dest=$1
 
-cp target/release/hs ~/bin
+[ -z $Dest ] && Dest="$HOME/bin"
+if [ ! -d $Dest ]; then
+  echo Directory $Dest does not exist, attempting to create it
+  mkdir -p $Dest
+fi
+
+echo Installing to: $Dest/hs
+cargo build --release
+cp target/release/hs $Dest
