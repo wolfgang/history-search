6813fd8425a663fbbf2ae1304e64aa2524afd418
wolfgang <wolfgang.deutsch@gmail.com>
2021-04-07 09:51:28 +0200

    Extract helper function to create view

--- src/_tests/item_list_view_test.rs
+++ src/_tests/item_list_view_test.rs
@@ -22,11 +22,10 @@ mod refresh {
     #[test]
     fn render_empty_prompt_if_no_items() -> crossterm::Result<()> {
         let mut stdout_spy = StdoutSpy::new();
-        let display_height = 4;
-        let display_width = 10;
-        let mut view = ItemListView::new(display_width, display_height, &mut stdout_spy);
         let items = Vec::new();
-        let model = ItemListModel::new(&items);
+        let mut view = make_10x4_view(&mut stdout_spy);
+        let model = make_model(&items);
+
         view.refresh(&model)?;
         let clear = f!("{esc}[0G          \n\r          \n\r          \n\r          \n\r{esc}[4A");
         let render_prompt = f!("{esc}[0G{esc}7> \n\r");
@@ -41,12 +40,9 @@ mod refresh {
     #[test]
     fn render_all_items_if_no_search_term() -> crossterm::Result<()> {
         let mut stdout_spy = StdoutSpy::new();
-        let display_height = 4;
-        let display_width = 10;
-        let mut view = ItemListView::new(display_width, display_height, &mut stdout_spy);
         let items = vec!["one".into(), "two".into()];
-        let mut model = ItemListModel::new(&items);
-        model.set_selection_window_height(10);
+        let mut view = make_10x4_view(&mut stdout_spy);
+        let model = make_model(&items);
 
         view.refresh(&model)?;
         let clear = f!("{esc}[0G          \n\r          \n\r          \n\r          \n\r{esc}[4A");
@@ -64,12 +60,9 @@ mod refresh {
     #[test]
     fn render_search_term_and_matching_items() -> crossterm::Result<()> {
         let mut stdout_spy = StdoutSpy::new();
-        let display_height = 4;
-        let display_width = 10;
-        let mut view = ItemListView::new(display_width, display_height, &mut stdout_spy);
         let items = vec!["one".into(), "tree".into(), "palm tree".into()];
-        let mut model = ItemListModel::new(&items);
-        model.set_selection_window_height(10);
+        let mut view = make_10x4_view(&mut stdout_spy);
+        let mut model = make_model(&items);
 
         model.add_to_search_term('t');
         model.add_to_search_term('r');
@@ -87,4 +80,16 @@ mod refresh {
         {restore_cursor}"));
         Ok(())
     }
+}
+
+fn make_10x4_view(stdout_spy: &mut StdoutSpy) -> ItemListView<StdoutSpy> {
+    let display_width = 10;
+    let display_height = 4;
+    ItemListView::new(display_width, display_height, stdout_spy)
+}
+
+fn make_model(items: &Vec<String>) -> ItemListModel {
+    let mut model = ItemListModel::new(items);
+    model.set_selection_window_height(10);
+    model
 }
